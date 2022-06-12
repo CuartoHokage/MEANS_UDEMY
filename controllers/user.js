d87ecec810de7d0e2a7896fcc667af77a6a3c672
CuartoHokage <jaime199505@hotmail.com>
2022-06-11 20:00:17 -0500

    pendiente clase 154 subida de archivos para avatars

--- controllers/user.js
+++ controllers/user.js
@@ -5,6 +5,7 @@ var bcrypt= require('bcrypt-nodejs');
 var User= require('../models/user');
 var jwt= require('../services/jwt');
 const { param } = require('../routes/user');
+const user = require('../models/user');
 var controller={
     probando: function(req, res){
         return res.status(200).send({
@@ -21,10 +22,18 @@ var controller={
         //recoger parametros de peticion
         var params= req.body;
         //validar los datos
-        var validate_name=!validator.isEmpty(params.name);
-        var validate_surname=!validator.isEmpty(params.surname);
-        var validate_email=!validator.isEmpty(params.email) && validator.isEmail(params.email);
-        var validate_password=!validator.isEmpty(params.password);
+        try {
+            var validate_name=!validator.isEmpty(params.name);
+            var validate_surname=!validator.isEmpty(params.surname);
+            var validate_email=!validator.isEmpty(params.email) && validator.isEmail(params.email);
+            var validate_password=!validator.isEmpty(params.password);
+        } catch (error) {
+            return res.status(200).send({
+                message: 'Faltan datos por enviar',
+                params
+            });
+        }
+        
 
         if(validate_name && validate_surname && validate_email && validate_password){
             //crear objeto de usuario
@@ -94,8 +103,16 @@ var controller={
         var params= req.body;
 
         //Validar los datos
-        var validate_email=!validator.isEmpty(params.email) && validator.isEmail(params.email);
-        var validate_password=!validator.isEmpty(params.password);
+        try {
+            var validate_email=!validator.isEmpty(params.email) && validator.isEmail(params.email);
+            var validate_password=!validator.isEmpty(params.password);
+        } catch (error) {
+            return res.status(200).send({
+                message: 'Faltan datos por enviar',
+                params
+            });
+        }
+        
 
         if(!validate_email || !validate_password){
             return res.status(200).send({
@@ -148,11 +165,92 @@ var controller={
             
     },
     update: function(req, res){
-        return res.status(200).send({
-            //Crear middleware para comprobar el jwt token, ponerselo a la ruta
+        //Crear middleware para comprobar el jwt token, ponerselo a la ruta
             
-            message: 'Metodo de actualizacion'
-        })
+        //Recoger datos del usuario
+        var params=req.body
+        
+        //Validar datos
+        try{
+            var validate_name=!validator.isEmpty(params.name);
+            var validate_surname=!validator.isEmpty(params.surname);
+            var validate_email=!validator.isEmpty(params.email) && validator.isEmail(params.email);
+        }
+        catch(err){
+            return res.status(200).send({
+            
+                message: 'Faltan datos por enviar',
+                params
+            });
+        }
+        //Eliminar propiedades innecesarias
+        delete params.password;
+        
+        //Buscar y actualizar documento de la base de datos
+        var userId= req.user.sub
+        
+        //Comprobar si el email es unico
+        if(req.user.email!= params.email){ //condicion esta mal si se modifica correo a uno no existente no entra al else
+            console.log(req.user.email)
+            //console.log(params.email)
+            User.findOne({email: params.email.toLowerCase()}, (err, user)=>{
+                    if(err){
+                        return res.status(500).send({
+                            message: "Error al intentar identificarse"
+                        });    
+                    }
+                    if(user && user.email == params.email){
+                        
+                        return res.status(200).send({
+                            message: "El email esta en uso no puede ser modificado"
+                        });
+                    }
+                    console.log('sss')
+                });
+        }else{
+                User.findOneAndUpdate({_id:userId}, params, {new:true }, (err, userUpdate)=>{
+                    if(err){
+                        return res.status(500).send({
+                            status: "error",
+                            message: "Error al actualizar usuario"
+                        });
+                    }
+                    if (!userUpdate) {
+                        return res.status(200).send({
+                            status: "error",
+                            message: "No se actualizado usuario"
+                        });
+                    }
+                    //Devolver una respuesta
+                    return res.status(200).send({
+                        status: 'success',
+                        user: userUpdate
+                    });
+                });
+            }
+    },
+
+    uploadAvatar: function(req, res){
+        //Configurar el modulo multiparty (ad)
+
+        //Recoger fichero de la peticion 
+        var file_name = 'Avatar no subido';
+        
+        console.log(req.files)
+        //Conseguir el nombre y extension de archivo subido
+
+        //Comprobar extension (solo imagenes), si no es valida extension borrar fichero
+
+        //Comprobar ID de usuario identificado
+
+        //Buscar y actualizar documento de BD
+
+
+        //Devolver una respuesta
+        return res.status(200).send({
+            status: 'success',
+            message: 'Upload Avatar'
+        });
     }
 };
 
--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,8 @@ var UserController= require('../controllers/user');
 
 var router= express.Router();
 var md_auth =require('../middlewares/authenticated');
+var multiparty=require('connect-multiparty')
+var md_upload= multiparty({uploadDir: './uploads/users'});
 //rutas de prueba
 router.get('/probando', UserController.probando);
 router.post('/testeando', UserController.testeando);
@@ -13,5 +15,5 @@ router.post('/testeando', UserController.testeando);
 router.post('/register', UserController.save);
 router.post('/login', UserController.login);
 router.put('/update',md_auth.authenticated,UserController.update);
-
+router.post('/upload-avatar',[md_auth.authenticated, md_upload], UserController.uploadAvatar);
 module.exports= router;
